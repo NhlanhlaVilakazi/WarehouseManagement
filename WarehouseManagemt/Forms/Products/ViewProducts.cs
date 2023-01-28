d256a9159aaf6beefd48ac88a5ed4933a72dde13
Nhlanhla Vilakazi <vilakazinhlanhlenhle@gmail.com>
2023-01-29 00:01:07 +0200

    Minor bug fix

--- WarehouseManagemt/Forms/Products/ViewProducts.cs
+++ WarehouseManagemt/Forms/Products/ViewProducts.cs
@@ -1,24 +1,20 @@
 ﻿using WarehouseManagent.Business;
-using WarehouseManagent.Helpers;
 
 namespace WarehouseManagent.Forms.Products
 {
     public partial class ViewProductsForm : Form
     {
         private ProductBusiness productBusiness;
-        private PrimaryKeyFields keyFields;
         public ViewProductsForm()
         {
             InitializeComponent();
             productBusiness = new();
-            keyFields = new();
         }
 
         private void ViewProductsForm_Load(object sender, EventArgs e)
         {
             var products = productBusiness.GetProducts();
             productGridView.DataSource = products;
-            keyFields.HideFields(productGridView, 3);
         }
     }
 }
--- WarehouseManagemt/Forms/Suppliers/ViewSuppliers.cs
+++ WarehouseManagemt/Forms/Suppliers/ViewSuppliers.cs
@@ -1,26 +1,22 @@
 ﻿using WarehouseManagent.Business;
-using WarehouseManagent.Helpers;
 
 namespace WarehouseManagent.Forms.Supplier
 {
     public partial class ViewSuppliersForm : Form
     {
         private SupplierBusiness supplierBusiness;
-        private PrimaryKeyFields keyFields;
 
 
         public ViewSuppliersForm()
         {
             InitializeComponent();
             supplierBusiness = new();
-            keyFields = new();
         }
 
         private void ViewSuppliersForm_Load(object sender, EventArgs e)
         {
             var suppliers = supplierBusiness.GetSuppiers();
             supplierGridView.DataSource = suppliers;
-            keyFields.HideFields(supplierGridView, 1);
         }
     }
 }
